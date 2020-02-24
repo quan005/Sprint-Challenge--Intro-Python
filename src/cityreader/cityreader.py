f6f07c1857504b71ba7ed28acc32020909c3c7c7
DaQuan Freeman <ralphealfreeman@gmail.com>
2020-02-23 18:21:30 -0700

    Sprint Intro Python MVP Complete

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,16 @@
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f'"{self.name}", {self.lat},{self.lon}'
+    
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -20,6 +31,21 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+  with open('src\cityreader\cities.csv', 'r') as csvfile:
+    read = csv.reader(csvfile, delimiter=',')
+    line_count = 0
+
+    for row in read:
+      if line_count == 0:
+        line_count += 1
+      else:
+        city = row[0]
+        lat = float(row[3])
+        lon = float(row[4])
+        cities.append(City(city, lat,lon))
+        line_count += 1
+      # print(row)
     
     return cities
 
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    # This is the bottom base of all classes
+    def __init__(self):
+        pass
+
+class FlightVehicle(Vehicle):
+    # Vehicle is the base of this class
+    def __init__(self):
+        pass
+
+class Starship(FlightVehicle):
+    # FlightVehicle is the base of this class
+    def __init__(self):
+        pass
+
+class Airplane(FlightVehicle):
+    # FlightVehicle is the base of this class
+    def __init__(self):
+        pass
+
+class GroundVehicle(Vehicle):
+    # Vehicle is the base of this class
+    def __init__(self):
+        pass
+
+class Car(GroundVehicle):
+    # GroundVehicle is the base of this class
+    def __init__(self):
+        pass
+
+class Motorcycle(GroundVehicle):
+    # GroundVehicle is the base of this class
+    def __init__(self):
+        pass
